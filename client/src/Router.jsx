a4b6a23406c04c282475b9f9c9d40d576b2ce1cb
HISHAN03 <moidinhishan@gmail.com>
2023-11-23 00:21:27 +0530

    404-page-added

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./app/App"
 import Lobby from "./lobby/Lobby"
+import NotFound from "./NoPage/index";
 
 export default function Router() {
   return (
@@ -9,6 +10,7 @@ export default function Router() {
     <Routes>
       <Route path="/" element={<Lobby />} />
       <Route path="/home" element={<App />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   </BrowserRouter>
   );
